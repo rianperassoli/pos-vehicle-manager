2961b95b05f157ce42239c734f6d01d765d16c48
Rian Perassoli <rian.perassoli@gmail.com>
2018-07-25 22:23:57 -0300

    Criacao do projeto e conexao com o banco de dados mariadb. Configurado o pom.xml e application.properties

---
+++ src/main/java/br/com/rianperassoli/vehiclemanager/VehicleManagerApplication.java
@@ -0,0 +1,12 @@
+package br.com.rianperassoli.vehiclemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleManagerApplication.class, args);
+	}
+}
